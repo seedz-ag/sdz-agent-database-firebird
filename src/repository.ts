2ff23a4786098cdcd07063fc3870c0ba1e0af4ab
rubens.curvello <rubens.curvello@hotmail.com>
2022-01-25 19:03:34 -0300

    statement select first and skip firebird

--- src/repository.ts
+++ src/repository.ts
@@ -3,7 +3,13 @@ import { AbstractRepository } from "sdz-agent-types";
 export default class SQLRepository extends AbstractRepository {
     execute(query: string, page?: number, limit?: number): Promise<any> {
 
-    const newQuery = "undefined" !== typeof page && limit ?  this.buildQuery(query).replace(/^SELECT/gi, `SELECT FIRST  ${limit}  SKIP  ${page*limit}`) : this.buildQuery(query);
-    return this.getConnector().execute(newQuery);
+      const statement = [
+        "SELECT",
+        limit && `FIRST ${limit}`,
+        limit && page && `SKIP ${limit * page}`,
+        this.buildQuery(query).replace(/^SELECT/gi, '')
+        ].filter(v => !!v).join(' ')
+
+      return this.getConnector().execute(statement);
   }
 }
